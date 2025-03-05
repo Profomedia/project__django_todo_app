e2a950da083f84ee870cc3fe0490c02dd983a874
Profomedia <profogitrepos@gmail.com>
2025-03-05 15:35:44 +0300

    create user-form and submitting data to database

--- Todo/authenticate/views.py
+++ Todo/authenticate/views.py
@@ -1,8 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from . import models
+from authenticate.models import TODO
+
 
 # Create your views here.
 def home(req):
     return render(req, 'index.html')
 
 def signup(req):
+    if req.method=='POST':
+        fnm=req.POST.get('fnm')
+        emailId=req.POST.get('email')
+        pwd=req.POST.get('pwd')
+        __user= User.objects.create_user(fnm,emailId,pwd)
+        __user.save()
+        return redirect('login')
+    
+        print(fnm, emailId, pwd)
     return render(req,'authenticate/register.html')
+
+def login(req):
+    return render(req, 'authenticate/login.html')
