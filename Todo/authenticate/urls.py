4e6960e53639b1fba65923f0422fbd30a2a7d8df
Profomedia <profogitrepos@gmail.com>
2025-03-05 17:26:39 +0300

    added login form

--- Todo/authenticate/urls.py
+++ Todo/authenticate/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('',views.home, name='home'),
+    path('todo/',views.todo, name='todo'),
     path('signup/',views.signup, name='signup'),
-    path('login/',views.login, name='login')
+    path('login/',views.signin, name='login'),
 ]
--- Todo/authenticate/views.py
+++ Todo/authenticate/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from . import models
 from authenticate.models import TODO
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -15,10 +17,25 @@ def signup(req):
         pwd=req.POST.get('pwd')
         __user= User.objects.create_user(fnm,emailId,pwd)
         __user.save()
+        
         return redirect('login')
     
-        print(fnm, emailId, pwd)
     return render(req,'authenticate/register.html')
 
-def login(req):
-    return render(req, 'authenticate/login.html')
+def signin(req):
+    if req.method=='POST':
+        fnm=req.POST.get('fnm')
+        pwd=req.POST.get('pwd')
+        __user=authenticate(req,username=fnm,password=pwd)
+        if __user is not None:
+            login(req,__user)
+            return redirect('todo')
+        else:
+            return redirect('login')
+
+    return render(req,'authenticate/login.html')
+
+
+@login_required(login_url="login")
+def todo(req):
+    return render(req, 'todo/todo.html')
