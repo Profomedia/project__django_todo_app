9c6e0b8a9019e384e863759e95ba3fb1c0387333
Profomedia <profogitrepos@gmail.com>
2025-03-05 14:57:35 +0300

    register site models.TODO and created super user

--- Todo/authenticate/views.py
+++ Todo/authenticate/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(req):
     return render(req, 'index.html')
+
+def signup(req):
+    return render(req,'authenticate/register.html')
